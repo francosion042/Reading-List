b2fcec0d7f99eb587be596173d5c28e7c75d487f
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2020-05-27 18:36:29 +0100

    ft-01-UI setup done, intro to Context API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
-
+import { ThemeContext } from "../contexts/ThemeContext";
 export default class BookList extends Component {
   render() {
     return (
-      <div>
-        <p>List Items</p>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, background: theme.bg }}>
+              <ul>
+                <li style={{ background: theme.ui }}>List item one</li>
+                <li style={{ background: theme.ui }}>List item one</li>
+                <li style={{ background: theme.ui }}>List item one</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 export default class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div>
-        <nav style={{ background: theme.ui, color: theme.syntax }}>
-          <h1>Reading List App</h1>
-        </nav>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div>
+              <nav style={{ background: theme.ui, color: theme.syntax }}>
+                <h1>Reading List App</h1>
+              </nav>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
